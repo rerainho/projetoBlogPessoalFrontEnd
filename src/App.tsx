20a6ea3bc04b358cf062a9a21c7ae0ce7f97c9a5
Renata Rainho <renata.rainho95@gmail.com>
2024-08-02 18:49:19 -0300

    :tada:	Iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// Elemento JSX/TSX => Função que retorna HTML
+
+import Home from "./pages/Home/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <h1>Teste 1</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+function Home(){
+ return (
+    <>
+        <h1>Página Home</h1>
+        <h2>Página principal do Blog Pessoal</h2>
+    </>
+ )
+}
+
+export default Home;
